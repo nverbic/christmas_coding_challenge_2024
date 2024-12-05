bd6a84bdc6b2c426efc97543f1c85d30aea10917
Nevena Verbic <nevena.verbic@gmail.com>
2024-12-05 17:54:22 +0100

    Day 04 Add faster solution for leetcode task 169: majority element

--- src/day_04/majority_element.py
+++ src/day_04/majority_element.py
@@ -20,9 +20,11 @@ Majority elem in the array: 2
 """
 
 from typing import List
+from collections import Counter
 
 class MajorityElement(object):
     def majorityElement(self, nums: List[int]) -> int:
+        """ Find majority element """
         n = len(nums)
         nums.sort()
         count=1
@@ -35,10 +37,17 @@ class MajorityElement(object):
                 count=1
         return nums[0]
 
+    def majorityElementCounter(self, nums: List[int]) -> int:
+        """ Find majority element - faster solution """
+        counter_nums = Counter(nums)
+        val, _ = counter_nums.most_common(1)[0]
+        return val
+
 
 if __name__ == "__main__":
-    nums=[2,2,1]
+    nums=[2,1,2]
     majorElem = MajorityElement()
-    result = majorElem.majorityElement(nums)
+    #result = majorElem.majorityElement(nums)
+    result = majorElem.majorityElementCounter(nums)
     print(f"Array: {nums}")
     print(f"Majority elem in the array: {result}")
